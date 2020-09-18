f7924b251ba9e0d72bc116bec7c2776b8b8ac4e8
joshuayannix <joshuayannix@gmail.com>
2020-09-17 21:47:24 -0400

    localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -147,10 +147,6 @@ elements.shopping.addEventListener('click', e => {
  * LIKES Controller
  */
 
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
   // Create a new likes object (if it's not already there)
   if (!state.likes) state.likes = new Likes();
@@ -186,7 +182,19 @@ const controlLike = () => {
   likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
 
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
